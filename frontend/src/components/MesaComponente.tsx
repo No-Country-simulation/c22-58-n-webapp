f39de54dfb115452720d51c2314a494520391cee
RuxCastillo <rubenldc1412@gmail.com>
2024-11-24 13:19:13 -0600

    centrado los numeros de las mesas

--- frontend/src/components/MesaComponente.tsx
+++ frontend/src/components/MesaComponente.tsx
@@ -1,7 +1,7 @@
 export default function MesaComponente({ x, y, numero }: any) {
 	return (
 		<div
-			className="w-1/3 rounded-full bg-gray-500 h-1/2 text-center text-white leading-10 text-xl "
+			className="w-1/3 rounded-full bg-gray-500 h-1/2 text-white leading-10 text-xl flex justify-center items-center"
 			style={{ gridColumn: x, gridRow: y }}
 		>
 			{numero}
