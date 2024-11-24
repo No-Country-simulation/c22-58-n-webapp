f975b6899c5de123a699df8b9493e0e186bf77b2
RuxCastillo <rubenldc1412@gmail.com>
2024-11-24 14:24:39 -0600

    instalado el react router y configurado con las vistas que se tienen actualmente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import './index.css';
-//import BarraNavegacionMenu from './components/BarraNavegacionMenu';
-//import Cocina from './components/Cocina';
+import BarraNavegacionMenu from './components/BarraNavegacionMenu';
+import Cocina from './components/Cocina';
 import Manager from './components/Manager';
 import Mesas from './components/Mesas';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	return <Mesas />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/mesas" element={<Mesas />} />
+				<Route path="/cocina" element={<Cocina />} />
+				<Route path="/manager" element={<Manager />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
