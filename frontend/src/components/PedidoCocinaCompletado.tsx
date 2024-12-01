68d22f48fdb510a1989e525839d2af7a4fa106f0
RuxCastillo <rubenldc1412@gmail.com>
2024-12-01 14:31:08 -0600

    actualizado cocina con zustand para guardar el estado globalmente

--- frontend/src/components/PedidoCocinaCompletado.tsx
+++ frontend/src/components/PedidoCocinaCompletado.tsx
@@ -1,22 +1,19 @@
 function PedidoCocinaCompletado({ orden }) {
-	return (
-		<section className="flex flex-col relative h-40 bg-[#d7e7ff] mt-3 p-2 md:w-full xl:h-52 2xl:w-72">
-			<header className="flex mb-2">
-				<span className="mr-auto ml-2 text-sm lg:text-lg">{orden.id}</span>{' '}
-				<span className="mr-2 text-sm lg:text-lg">{orden.mesa}</span>
-			</header>
-			
-			<main>
-				<ul className="text-sm lg:text-lg lg:pt-3">
-				{orden.items.map((item, index) => (
-					<li key={index}>{item}</li>
-				))}
-				</ul>
-      		</main>
-			<footer className="absolute bottom-2 right-2 bg-[#d7e7ff] text-slate-500">
-				<span>12:36 PM</span>
-			</footer>
-		</section>
-	);
+  return (
+    <section className="xl:h-42 relative mt-3 flex h-36 flex-col bg-[#d7e7ff] p-2 md:w-full 2xl:w-72">
+      <header className="mb-2 flex">
+        <span className="ml-2 mr-auto text-sm lg:text-lg">{orden.id}</span>{' '}
+        <span className="mr-2 text-sm lg:text-lg">{orden.mesa}</span>
+      </header>
+
+      <main>
+        <ul className="lg:text-md text-sm lg:pt-3">
+          {orden.items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </main>
+    </section>
+  );
 }
 export default PedidoCocinaCompletado;
