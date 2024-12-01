152c407a43efa4e644d62ea3b17e82dfd974cb6f
RuxCastillo <rubenldc1412@gmail.com>
2024-12-01 15:18:37 -0600

    seleccion de mesas acomodado

--- frontend/store/Mesas.tsx
+++ frontend/store/Mesas.tsx
@@ -1,69 +1,68 @@
 import { create } from 'zustand';
 
 const useMesas = create((set) => ({
-	areas: [
-		{
-			nombreArea: 'principal',
-			mesas: [
-				{
-					id: 1,
-					x: 3,
-					y: 2,
-				},
-				{
-					id: 2,
-					x: 5,
-					y: 7,
-				},
-				{
-					id: 3,
-					x: 1,
-					y: 4,
-				},
-			],
-		},
-		{
-			nombreArea: 'exterior',
-			mesas: [
-				{
-					id: 1,
-					x: 7,
-					y: 1,
-				},
-				{
-					id: 2,
-					x: 3,
-					y: 8,
-				},
-				{
-					id: 3,
-					x: 5,
-					y: 7,
-				},
-			],
-		},
-		{
-			nombreArea: 'planta alta',
-			mesas: [
-				{
-					id: 1,
-					x: 3,
-					y: 3,
-				},
-				{
-					id: 2,
-					x: 4,
-					y: 3,
-				},
-				{
-					id: 3,
-					x: 5,
-					y: 3,
-				},
-			],
-		},
-	],
+  areas: [
+    {
+      nombreArea: 'principal',
+      mesas: [
+        {
+          id: 1,
+          x: 3,
+          y: 2,
+        },
+        {
+          id: 2,
+          x: 5,
+          y: 7,
+        },
+        {
+          id: 3,
+          x: 1,
+          y: 4,
+        },
+      ],
+    },
+    {
+      nombreArea: 'exterior',
+      mesas: [
+        {
+          id: 4,
+          x: 7,
+          y: 1,
+        },
+        {
+          id: 5,
+          x: 3,
+          y: 8,
+        },
+        {
+          id: 6,
+          x: 5,
+          y: 7,
+        },
+      ],
+    },
+    {
+      nombreArea: 'planta alta',
+      mesas: [
+        {
+          id: 7,
+          x: 3,
+          y: 3,
+        },
+        {
+          id: 8,
+          x: 4,
+          y: 3,
+        },
+        {
+          id: 9,
+          x: 5,
+          y: 3,
+        },
+      ],
+    },
+  ],
 }));
 
 export default useMesas;
-
