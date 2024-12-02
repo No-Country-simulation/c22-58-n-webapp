22778282ffd018c4db8b7268af96d9aa72030fe4
RuxCastillo <rubenldc1412@gmail.com>
2024-12-02 14:20:47 -0600

    acomode el layout de layoutadmin para agregar el componente que crea el pedido

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Seleccion from './components/Seleccion';
 import Cuentas from './components/cuentas/Cuentas';
+import Menu from './components/Menu';
 
 //import BarraNavegacionMenu from './components/BarraNavegacionMenu';
 
@@ -22,40 +23,37 @@ import CardCategoria3 from './components/categorias/CardCategoria3';
 // Component
 import Home from './pages/admin/Home';
 
-
 // Pages
 import Error404 from './pages/Error404';
 
-
 // Layouts
 import LayoutAdmin from './layouts/LayoutAdmin';
-
 
 // axios.defaults.baseURL = "URL DEL DEPLOY"
 axios.defaults.baseURL = 'http://localhost:3000';
 
 function App() {
-	return (
-		<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<Login />} />
-			<Route path="/register" element={<Register />} />
-       		<Route path="/seleccion" element={<Seleccion />} />
-			<Route path="/mesas" element={<Mesas />} />
-			<Route path="/cocina" element={<Cocina />} />
-			<Route path="/manager" element={<Manager />} />
-			<Route path="/categorias" element={<LayoutAdmin />}>
-				<Route index element={<Home />} />
-				<Route path="categoria1" element={<CardCategoria1 />} />
-				<Route path="categoria2" element={<CardCategoria2 />} />
-				<Route path="categoria3" element={<CardCategoria3 />} />
-			</Route>
-			<Route path="/cuentas/:numeroMesa/:totalCuenta" element={<Cuentas />} />
-			<Route path='*' element={<Error404 />} />
-			 
-		</Routes>
-	 </BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/seleccion" element={<Seleccion />} />
+        <Route path="/mesas" element={<Mesas />} />
+        <Route path="/cocina" element={<Cocina />} />
+        <Route path="/manager" element={<Manager />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/categorias" element={<LayoutAdmin />}>
+          <Route index element={<Home />} />
+          <Route path="categoria1" element={<CardCategoria1 />} />
+          <Route path="categoria2" element={<CardCategoria2 />} />
+          <Route path="categoria3" element={<CardCategoria3 />} />
+        </Route>
+        <Route path="/cuentas/:numeroMesa/:totalCuenta" element={<Cuentas />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/layouts/LayoutAdmin.tsx
+++ frontend/src/layouts/LayoutAdmin.tsx
@@ -1,20 +1,23 @@
-import { Outlet } from "react-router-dom"; //el outlet nos sirve para agregarle un componente dentro de este componente
+import { Outlet } from 'react-router-dom'; //el outlet nos sirve para agregarle un componente dentro de este componente
 
 // Componentes
-import SideBar from "../components/Sidebar";
-import BarraNavegacionMenu from "../components/BarraNavegacionMenu";
+import SideBar from '../components/Sidebar';
+import BarraNavegacionMenu from '../components/BarraNavegacionMenu';
 
 function LayoutAdmin() {
-    return (
-       <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
-            <SideBar />
-            <div className="w-full xl:col-span-5">
-                <BarraNavegacionMenu />
-                <div className="h-[90vh] overflow-y-scroll">
-                    <Outlet />
-                </div>                
-            </div>
-       </div>
-    );
+  return (
+    <div className="grid h-screen grid-cols-1 grid-rows-[10%,90%] xl:grid-cols-6">
+      <SideBar />
+      <div className="col-start-1 col-end-7 row-start-1 w-full">
+        <BarraNavegacionMenu />
+      </div>
+      <div className="col-start-2 col-end-5 row-start-2 row-end-3 h-[90vh] overflow-y-scroll">
+        <Outlet />
+      </div>
+      <div className="col-start-5 col-end-7 row-start-2 bg-slate-400">
+        aqui va el componente donde se pone el pedido
+      </div>
+    </div>
+  );
 }
 export default LayoutAdmin;
