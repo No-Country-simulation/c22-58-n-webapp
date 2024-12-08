ba3472e0409b7a53a3a5f4871b626375769549d5
Oscar Lozano <olozano1194@gmail.com>
2024-12-08 14:51:13 -0500

    Se agregaron los cambios que hicieron en la rama develop para este componente

--- frontend/src/components/BarraNavegacionMenu.tsx
+++ frontend/src/components/BarraNavegacionMenu.tsx
@@ -1,8 +1,11 @@
+import useMesas from "../../store/Mesas";
+
 function BarraNavegacionMenu() {
+	const mesaActual = useMesas((state: any) => state.mesaSeleccionada);
 	return (
 		<header className="flex justify-between border-b-gray-500 h-24 border-solid border-2 items-center font-sans">
 			<h1 className="uppercase md:mr-auto md:ml-28 font-bold text-xl md:text-2xl">dishflow</h1>
-			<span className="uppercase md:mx-28 font-bold text-sm">numero de mesa</span>
+			<span className="uppercase md:mx-28 font-bold text-sm">Mesa: {mesaActual}</span>
 			<span className="uppercase md:mx-28 font-bold text-sm">
 				nombre del mesero
 			</span>
