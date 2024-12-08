1891c62961483f51a6f7a8b253e6a9b64bea267e
Oscar Lozano <olozano1194@gmail.com>
2024-12-08 16:24:39 -0500

    Agregando los cambios para que esta rama quede a la par con los cambios de la rama develop

--- frontend/src/components/categorias/CardCategoria1.tsx
+++ frontend/src/components/categorias/CardCategoria1.tsx
@@ -7,66 +7,41 @@ function CardCategoria1() {
     const ordenes = useOrdenActual((state: any) => state);
 
     return (
-        <>
+        
         <section className="flex flex-col justify-center items-center flex-wrap gap-3 mt-5 p-4">
             <div>
                 <h1 className="text-xl text-slate-900 font-bold text-center pt-2 pb-5 lg:text-4xl">Menú Destacado</h1>
             </div>
 
-            {elMenu.map((element: any, idx) => (
-                <div className="w-full flex flex-col justify-center items-center flex-wrap gap-5 md:justify-between md:flex-row lg:gap-9">
-
-                <div className="card w-[85%] border-double border-4 border-stone-700 flex flex-col gap-2 md:w-[46%] xl:w-[31%]">
-                    <Link to='/categorias'>
-                        <div className="card-body p-2">
-                            <img className="w-full h-28 lg:h-36" src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg" alt="Imagen de los platos" />
-                            <h5 className="card-title text-md font-bold text-slate-700  text-center pt-3 lg:text-lg xl:text-xl">Nombre del plato</h5>
-                            <p className="text-end text-slate-700 text-md font-semibold pt-3 lg:text-lg xl:text-xl">$100.00</p>
-                        </div>
-                    </Link>
-                    
-                </div>
-                <div className="card w-[85%]  border-double border-4 border-stone-700 flex flex-col gap-2 md:w-[46%] xl:w-[31%]">
-                    <div className="card-body p-2">
-                        <img className="w-full h-28 lg:h-36" src="https://cdn.pixabay.com/photo/2022/01/20/08/24/fish-6951734_1280.jpg" alt="Imagen de los platos" />
-                        <h5 className="card-title text-md font-bold text-slate-700 text-center pt-3 lg:text-lg xl:text-xl">Nombre del plato</h5>
-                        <p className="text-end text-slate-700 text-md font-semibold pt-3 lg:text-lg xl:text-xl">$100.00</p>
-
-                    </div>
-                </div>
-                <div className="card w-[85%]  border-double border-4 border-stone-700 flex flex-col gap-2 md:w-[46%] xl:w-[31%]">
-                    <div className="card-body p-2">
-                        <img className="w-full h-28 lg:h-36" src="https://cdn.pixabay.com/photo/2017/03/13/13/39/pancakes-2139844_1280.jpg" alt="Imagen de los platos" />
-                        <h5 className="card-title text-md font-bold text-slate-700  text-center pt-3 lg:text-lg xl:text-xl">Nombre del plato</h5>
-                        <p className="text-end text-slate-700 text-md font-semibold pt-3 lg:text-lg xl:text-xl">$100.00</p>
-
-                    </div>
-                </div>
-                <div className="card w-[85%]  border-double border-4 border-stone-700 flex flex-col gap-2 md:w-[46%] xl:w-[31%]">
-                    <div className="card-body p-2">
-                        <img className="w-full h-28 lg:h-36" src="https://cdn.pixabay.com/photo/2024/09/19/10/26/ai-generated-9058360_1280.jpg" alt="Imagen de los platos" />
-                        <h5 className="card-title text-md font-bold text-slate-700  text-center pt-3 lg:text-lg xl:text-xl">Nombre del plato</h5>
-                        <p className="text-end text-slate-700 text-md font-semibold pt-3 lg:text-lg xl:text-xl">$100.00</p>
-
-                    </div>
-                </div>
-                <div className="card w-[85%]  border-double border-4 border-stone-700 flex flex-col gap-2 md:w-[46%] xl:w-[31%]">
-                    <div className="card-body p-2">
-                        <img className="w-full h-28 lg:h-36" src="https://cdn.pixabay.com/photo/2014/04/22/02/56/pizza-329523_1280.jpg" alt="Imagen de los platos" />
-                        <h5 className="card-title text-md font-bold text-slate-700  text-center pt-3 lg:text-lg xl:text-xl">Nombre del plato</h5>
-                        <p className="text-end text-slate-700 text-md font-semibold pt-3 lg:text-lg xl:text-xl">$100.00</p>
-
-                    </div>
-                </div>
-
-            </div>
-
-            ))}
             
-        </section>
-        
-        </>
+                <div className="w-full flex flex-col justify-center items-center flex-wrap gap-5 md:justify-between md:flex-row lg:gap-9">
+                    {elMenu.map((element: any, idx) => (
+                        <div 
+                            className="card w-[85%] border-double border-4 border-stone-700 flex flex-col gap-2 md:w-[46%] xl:w-[31%]"
+                            key={idx}
+                            onClick={() => 
+                                ordenes.agregarALaOrden({
+                                    nombre: element.nombre,
+                                    precio: element.precio,
+                                })
+                            }
+                        >                    
+                        <div className="card-body p-2">
+                            <img 
+                                className="w-full h-28 lg:h-36" 
+                                src={element.imagen}
+                                alt={`Imagen de ${element.nombre}`}                            
+                            />
 
+                            <h5 className="card-title text-md font-bold text-slate-700  text-center pt-3 lg:text-lg xl:text-xl">{element.nombre}</h5>
+                            <p className="text-end text-slate-700 text-md font-semibold pt-3 lg:text-lg xl:text-xl">{element.precio}</p>
+                        </div>                    
+                        
+                        </div>
+
+                    ))}
+            </div>            
+        </section>
     );
     
 }
--- frontend/src/components/ordenActual/ordenActual.tsx
+++ frontend/src/components/ordenActual/ordenActual.tsx
@@ -1,20 +1,52 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 // Icons
 import { RiDeleteBin6Line } from "react-icons/ri";
 
+//Components
+import useOrdenActual from "../../../store/OrdenActual";
+import useMesas from "../../../store/Mesas";
+import usePedidos from "../../../store/Pedidos";
+
 const OrdenActual = () => {
-    //Estado para los productos en la orden
-    const [productos, setProductos] = useState([
-        { id: 1, nombre: "Producto 1", precio: 10 },
-        { id: 2, nombre: "Producto 2", precio: 20 },
-        { id: 3, nombre: "Producto 3", precio: 30 },
-    ]);
+    const mesa = useMesas((state: any) => state.mesaSeleccionada);
+    const agregarPedido = usePedidos((state) => state.agregarPedido);
+    const ordenes = useOrdenActual((state: any) => state);
+    const productos = ordenes.orden;
+    const navigate = useNavigate();
+
+    let total = 0;
+    let elPedido = {
+        id: Math.floor(Math.random() * 300000),
+        mesa: mesa,
+        items: arrayProductos(productos),
+        completado: false,
+    };
+    console.log(elPedido);
+    
+
+    const pedidos = usePedidos((state) => state.pedidos);
+    console.log(pedidos);
+    
+    function arrayProductos(productos) {
+        const newArray = [];
+        productos.forEach((producto) => {
+            newArray.push(producto.nombre);
+        });
+        return newArray;        
+    }
 
     //Función para eliminar el producto
-    const eliminarProducto = (id: number) => {
-        //Filtrar los productos para eliminar el que se seleccionó
-        setProductos(productos.filter((producto) => producto.id !== id));
+    const eliminarProducto = (id: string) => {
+        ordenes.quitarAlaOrden(id);
     };
+
+    function elTotal(element:any) {
+        element.forEach((element: any) => {
+            total += element.precio;            
+        });              
+    }
+    elTotal(productos)
 
     return (
         <main className="w-full flex justify-center items-center">        
@@ -26,32 +58,37 @@ const OrdenActual = () => {
                     {productos.length === 0 ? (
                         <p className="text-lg text-center pb-10 xl:text-4xl">No hay productos en la orden.</p>
                     ) : (
-                        productos.map(producto => (
-                        <div key={producto.id} className="flex justify-between items-center h-20 bg-sky-50 border-solid border-2 border-slate-400 gap-3 p-3 mb-4">
-                            <div className="">
-                            <h4 className="text-sm font-bold text-center lg:text-xl">{producto.nombre}</h4>
-                            <p className="text-center text-sm font-semibold lg:text-lg">${producto.precio}</p>
-                            </div>
-                            <button
-                                onClick={() => eliminarProducto(producto.id)}
+                        productos.map((producto: any, idx) => (
+
+                            <div key={idx} className="flex justify-between items-center h-20 bg-sky-50 border-solid border-2 border-slate-400 gap-3 p-3 mb-4">
+                                <div className="">
+                                    <h4 className="text-sm font-bold text-center lg:text-xl">{producto.nombre}</h4>
+                                    <p className="text-center text-sm font-semibold lg:text-lg">${producto.precio}</p>
+                                </div>
+                                <button
+                                onClick={() => eliminarProducto(producto.nombre)}
                                 className="flex justify-center items-center bg-red-500 text-white p-2 rounded lg:w-16 lg:h-10 lg:text-2xl"
-                            >
-                                <RiDeleteBin6Line />
-                            </button>
-                        </div>
+                                >
+                                    <RiDeleteBin6Line />
+                                </button>
+                            </div>
                         ))
                     )}
                 </div>
                 <footer className="border-2 border-solid border-gray-200 p-4 font-bold">
                     <div className="mb-4 flex justify-between">
                     <span>total</span>
-                    <span>$300.00</span>
+                    <span>${total}.00</span>
                     </div>
-                    <button className="h-10 w-full bg-red-500 text-xs rounded-md lg:text-xl uppercase text-white">
-                    enviar a cocina
+                    <button 
+                        className="h-10 w-full bg-red-500 text-xs rounded-md lg:text-xl uppercase text-white"
+                        onClick={() => agregarPedido(elPedido)}
+                    >
+                        enviar a cocina
                     </button>
                 </footer>
             </aside>
+            <button onClick={() => navigate('/cocina')}>Ir a cocina</button>
         </main>
     );
 }
